d0f54ab95948c8733d24e40c9354ca13dc4ca788
Christopher Roberson <roberson_cw@yahoo.com>
2018-05-11 10:57:52 -0500

    caps change

--- api/api.php
+++ api/api.php
@@ -6,7 +6,7 @@ use Api\Vendor as Vendor;
 
 // TODO: create a config file to store db configurations and other things
 
-$_CONFIG['env'] = 'dev';    //using 
+$_CONFIG['env'] = 'dev';
 if($_CONFIG['env'] == 'dev') {
     require_once 'Vendor/db_local.php';     //not in version control; it contains my local db connection info
 } else {
